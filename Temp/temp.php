03a10501ceed5ea1fcb2e1beba1673cfcb7a94a3
aticsia <aticsia@gmail.com>
2013-05-26 13:12:40 +0700

    change

--- Temp/temp.php
+++ Temp/temp.php
@@ -1,4 +1,13 @@
 <?php
-$text = 'Thisis a testiii';
-echo substr_count($text, 'i'); // 2
+$str ="
+Trái tim bạn màu xanh lá 
+Tình yêu giúp bạn hoàn thiện chính mình, nhưng không có nghĩa bạn phải cố đi tìm nó. Khi tình yêu xuất hiện, bạn biết cách dung hòa nó vào cuộc sống một cách êm đẹp.
+
+Phong cách iu của bạn: Thoải mái. 
+Cuộc hẹn đầu may mắn: Vừa đi dạo và tám chuyện. 
+Người trong mộng: Vừa nhiệt huyết vừa điềm đạm.
+
+Bạn mang đến cho tình yêu: Sự cân bằng. ";
+
+echo str_ireplace('\r','',$str);
 ?>
